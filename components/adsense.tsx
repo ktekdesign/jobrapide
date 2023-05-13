13a7bbf299da170d9a32073f17babb322e9c8377
ktekdesign <contact@ktekdesign.com>
2023-05-13 16:03:25 -0300

    fix: show adsense

--- components/adsense.tsx
+++ components/adsense.tsx
@@ -1,19 +1,15 @@
 import { Adsense } from '@ctrl/react-adsense'
 import { memo } from 'react'
 
-const GAdSense = ({ variant }: { variant?: string }) => {
-  if (process.env.NODE_ENV !== 'production') return <></>
-
-  return (
-    <div className="adsense">
-      <Adsense
-        client="ca-pub-6631438162509513"
-        slot={variant === 'sponsored' ? '2682415108' : '6940028987'}
-        style={{ display: 'block' }}
-        format={variant === 'sponsored' ? 'autorelaxed' : 'auto'}
-      />
-    </div>
-  )
-}
+const GAdSense = ({ variant }: { variant?: string }) => (
+  <div className="adsense">
+    <Adsense
+      client="ca-pub-6631438162509513"
+      slot={variant === 'sponsored' ? '2682415108' : '6940028987'}
+      style={{ display: 'block' }}
+      format={variant === 'sponsored' ? 'autorelaxed' : 'auto'}
+    />
+  </div>
+)
 
 export default memo(GAdSense)
