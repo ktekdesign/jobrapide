5d5d2f7eadbb2c50d8618ab7f6873da541cfe213
ktekdesign <contact@ktekdesign.com>
2023-05-28 16:22:34 -0300

    fix: dynamic import Modal

--- components/dynamic-navbar.tsx
+++ components/dynamic-navbar.tsx
@@ -1,26 +1,25 @@
-import { memo, useCallback, useState } from 'react'
+import { memo, useState, useTransition } from 'react'
 import dynamic from 'next/dynamic'
 
 import IconMenu from '@components/icons-menu'
 import SearchIconButton from '@components/search-icon-button'
 
-const Modal = dynamic(() => import('@components/modal'))
-const Search = dynamic(() => import('@components/search'))
+const Modal = dynamic(() => import('@components/modal'), { ssr: false })
+const Search = dynamic(() => import('@components/search'), { ssr: false })
 
 const DynamicNavBar = () => {
   const [open, setOpen] = useState(false)
+  const [isPending, startTransition] = useTransition()
 
-  const toggleModal = useCallback(() => {
-    setOpen(!open)
-  }, [open])
+  const toggleModal = () => startTransition(() => setOpen(!open))
 
   return (
     <>
       <IconMenu>
-        <SearchIconButton onClick={toggleModal} />
+        <SearchIconButton data-loading={isPending} onClick={toggleModal} />
       </IconMenu>
       {open && (
-        <Modal open={open} onClick={toggleModal}>
+        <Modal onClick={toggleModal}>
           <Search />
         </Modal>
       )}
--- components/modal.tsx
+++ components/modal.tsx
@@ -3,8 +3,8 @@ import { memo } from 'react'
 import CloseIcon from '/public/images/close.svg'
 import Button from '@components/form/Button'
 
-const Modal = ({ children, open, ...props }) => (
-  <dialog id="modal" open={open} {...props}>
+const Modal = ({ children, ...props }) => (
+  <dialog id="modal" open={true} {...props}>
     <div onClick={(e) => e.stopPropagation()} className="modal-inner">
       {children}
       <Button type="button" label="Close" className="closeButton" {...props}>
