aa8cf9a43dcfe21bececfa5d621a558cce14da53
Christian Kpanou <contact@ktekdesign.com>
2023-12-08 10:52:43 -0300

    fix: remove sample test

--- pages/secteur/[slug]/page/[id]Term.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Term from './[id]'
-
-describe('<Term />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Term />)
-  })
-})
